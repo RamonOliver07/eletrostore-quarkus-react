d497b1e99669d2585ac201ddd34e2ce5918b381b
Ramon Oliveira <ramonoliveira@MacBook-Pro-de-Ramon.local>
2025-07-15 15:04:53 -0700

    Corrige mais dependências e código do frontend

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  server: {
+    proxy: {
+      // Redireciona todas as requisições que começam com /api
+      '/api': {
+        target: 'http://localhost:8080', // O endereço do seu backend Quarkus
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
+})
